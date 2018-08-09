6cfa046ab2637ae62bc759a6338aa8eb9ee2e84c
mama frantz <hyunfrantz@gmail.com>
2018-08-08 21:52:20 -0400

    controller edit

--- app/controllers/userrepositorys_controller.rb
+++ app/controllers/userrepositorys_controller.rb
@@ -1,10 +1,10 @@
 class UserrepositorysController < ApplicationController
-
   def show
     username = params[:username]
     reponame = params[:reponame]
-    @user = User.where(username: username)
-    @repo = User.repos.find_by(reponame: reponame)
-
+    @user = User.find_by(username: username)
+    @repo = @user.repos.find_by(reponame: reponame)
   end
 end
+
+#difference between where and find_by
